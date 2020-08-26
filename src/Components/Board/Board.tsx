da04cf1244a3415e66af9abe87ec54d7ea826945
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-08-26 19:44:46 -0300

    BDraw condition implemented

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -7,18 +7,69 @@ const Board: React.FC = () => {
   const emptyBoard: Array<string> = Array(9).fill("");
   //tipando estado board como array de strings
   const [board, setBoard] = useState<Array<string>>(emptyBoard);
-  
+  //tipando estado currentPlayer como string
+  const [currentPlayer, setCurrentPlayer] = useState<string>('O');
+  //tipando estado currentPlayer como string
+  const [winner, setWinner] = useState<string>('');
 
-  const handleBoardClick = (index: number) => {
+  useEffect(checkWinner, [board]);
+
+  function handleBoardClick(index: number){
+    if (winner) {
+      console.log("Jogo finalizado!");
+      return null;
+    }
     //Verify if the player click on a fulfilled board field
     if (board[index] !== "") {
       return null;
     }
-    setBoard(
-      board.map((element, elementIndex) => elementIndex === index ? 'X' : element));
+    setBoard(board.map((element, elementIndex) => elementIndex === index ? currentPlayer : element));
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
 
+
+  function checkWinner(){
+
+    const winningPossibilities: string[][] = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    const winnerO: string[] = ["O", "O", "O"];
+    const winnerX: string[] = ["X", "X", "X"];
+
+    winningPossibilities.forEach((possibility) => {
+      if (possibility.toString() === winnerO.toString()) {
+        setWinner("O");
+        console.log("O venceu!");
+      } 
+      if (possibility.toString() === winnerX.toString()) {
+        setWinner("X");
+        console.log("X venceu!");
+      } 
+    });
+  }
+
+  function checkDraw() {
+    if (board.every((element) => element !== "") && winner === '') {
+      console.log('E');
+      setWinner("E");
+    }
+  }
+
+  checkDraw();
+
   return (
+    <>
+    {winner ?  <div className={`jogador-da-vez ${currentPlayer}`}>Jogo Finalizado</div> : <div className={`jogador-da-vez ${currentPlayer}`}>Jogador {currentPlayer} sua vez!</div>}
     <div className="container">
       <div className="game">
         <div className="board-grid">
@@ -26,6 +77,7 @@ const Board: React.FC = () => {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
