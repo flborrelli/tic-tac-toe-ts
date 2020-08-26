24ca12db9038a97b539a87ab718b1f8a1fd820f5
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-08-26 18:11:52 -0300

    Board component working well

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -7,21 +7,22 @@ const Board: React.FC = () => {
   const emptyBoard: Array<string> = Array(9).fill("");
   //tipando estado board como array de strings
   const [board, setBoard] = useState<Array<string>>(emptyBoard);
+  
 
+  const handleBoardClick = (index: number) => {
+    //Verify if the player click on a fulfilled board field
+    if (board[index] !== "") {
+      return null;
+    }
+    setBoard(
+      board.map((element, elementIndex) => elementIndex === index ? 'X' : element));
+  }
 
   return (
     <div className="container">
       <div className="game">
         <div className="board-grid">
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
-          <button className="btn">X</button>
+          {board.map((element, index) => <button className={`btn ${element}`} key={index} onClick={() => handleBoardClick(index)}>{element}</button>)}
         </div>
       </div>
     </div>
